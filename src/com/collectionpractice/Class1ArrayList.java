d1404c97108d074dd6409fc9bb3c3b6ed66db8e2
adityapratap52 <adityapratap1490@gmail.com>
2021-11-01 23:37:19 +0530

    Create Class1ArrayList.java class

---
+++ src/com/collectionpractice/Class1ArrayList.java
@@ -0,0 +1,12 @@
+package com.collectionpractice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Class1ArrayList {
+    public static void main(String[] args) {
+        List<Integer> myList = new ArrayList<>(20);
+        myList.add(4);
+        System.out.println(myList);
+    }
+}
