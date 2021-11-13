788b5f37622d34d6b3ec7e3abac8407122104378
adityapratap52 <adityapratap1490@gmail.com>
2021-11-13 13:31:50 +0530

    edit some code

--- src/com/collectionframework/arraysclass/AsListMethod.java
+++ src/com/collectionframework/arraysclass/AsListMethod.java
@@ -1,7 +1,11 @@
 package com.collectionframework.arraysclass;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class AsListMethod {
     public static void main(String[] args) {
-
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7);
+        System.out.println(list);
     }
 }
